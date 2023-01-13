e8c75446f16980cea064d9099a367a221bdff737
sgtPepper109 <a.r.shaikh1940@gmail.com>
2023-01-13 12:38:37 +0530

    attempt to configure mysql

--- GUI/src/app/home/home.component.ts
+++ GUI/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
+import { createPool, Pool } from 'mysql'
+
+let pool:Pool;
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-	submit():void  {
+export class HomeComponent{
+	submit(): void {
 		const trainRatio = parseInt((<HTMLInputElement>document.getElementById('traininginput')).value)
 		const testRatio = parseInt((<HTMLInputElement>document.getElementById('testinginput')).value)
 		const valRatio = parseInt((<HTMLInputElement>document.getElementById('valinput')).value)
-		console.log(trainRatio, testRatio, valRatio)
+		
+		try {
+			pool = createPool({
+				host: "localhost",
+				user: "root",
+				password: "1990",
+				database: "project"
+			});
+			console.log('MySQL adapter pool generated Successfully');
+		} catch (error) {
+			console.error('[mysql.connector][init][Error]: ', error);
+			throw new Error('failed to initialized pool');
+		}
 	}
 }
